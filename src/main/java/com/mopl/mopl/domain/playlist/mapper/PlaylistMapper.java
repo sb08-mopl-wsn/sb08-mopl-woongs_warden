cc6bf1fa68ed8443140f41fb26637f01bbaaf36a
idktomorrow <swj9675@naver.com>
2026-05-14 15:44:01 +0900

    feat: apply ContentSummary mapping to PlaylistDto and PlaylistMapper

--- src/main/java/com/mopl/mopl/domain/playlist/mapper/PlaylistMapper.java
+++ src/main/java/com/mopl/mopl/domain/playlist/mapper/PlaylistMapper.java
@@ -1,18 +1,23 @@
 package com.mopl.mopl.domain.playlist.mapper;
 
+import com.mopl.mopl.domain.content.dto.response.ContentSummary;
 import com.mopl.mopl.domain.content.mapper.ContentMapper;
 import com.mopl.mopl.domain.playlist.dto.response.PlaylistDto;
 import com.mopl.mopl.domain.playlist.entity.Playlist;
 import com.mopl.mopl.domain.user.mapper.UserMapper;
+import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = {UserMapper.class, ContentMapper.class})
 public interface PlaylistMapper {
 
+  @Mapping(source = "playlist.user", target = "owner")
+  @Mapping(source = "isSubscribed", target = "subscribedByMe")
+  @Mapping(source = "contents", target = "contents")
+  PlaylistDto toDto(Playlist playlist, Boolean isSubscribed, List<ContentSummary> contents);
+
   @Mapping(source = "user", target = "owner")
-  // TODO: findPlaylistById 구현 시, 이 매핑은 동적으로 처리되도록 수정 필요
   @Mapping(target = "subscribedByMe", constant = "true")
-  // TODO: ContentSummary DTO 구현 후, contents 필드 매핑 로직 추가 필요
   PlaylistDto toDto(Playlist playlist);
 }

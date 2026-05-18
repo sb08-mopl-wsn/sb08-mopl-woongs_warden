afed077a8ab81414c7af2bf08ec712d1949d5971
dev-jin8612 <workspace8612@gmail.com>
2026-05-18 17:34:17 +0900

    fix: coderabbit feedbag3

--- src/main/java/com/mopl/mopl/domain/auth/exception/AuthFailedRefrshToken.java
+++ src/main/java/com/mopl/mopl/domain/auth/exception/AuthFailedRefrshToken.java
@@ -2,6 +2,6 @@ package com.mopl.mopl.domain.auth.exception;
 
 public class AuthFailedRefrshToken extends AuthException {
     public AuthFailedRefrshToken() {
-        super(AuthErrorCode.AUTH_FAILED_REFRESH_TOEKN);
+        super(AuthErrorCode.AUTH_AUTHENTICATION_FAILED);
     }
 }

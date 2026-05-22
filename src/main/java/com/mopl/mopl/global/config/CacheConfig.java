44b64075941fd4c6d270119d25db8fc17182cc90
xxzeroeight <xxzeroeight@naver.com>
2026-05-22 13:06:14 +0900

    feat: migrate cache from Caffeine to Redis and add RedisTemplate configuration

--- src/main/java/com/mopl/mopl/global/config/CacheConfig.java
+++ src/main/java/com/mopl/mopl/global/config/CacheConfig.java
@@ -1,37 +1,39 @@
 package com.mopl.mopl.global.config;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
-import org.springframework.cache.transaction.TransactionAwareCacheManagerProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
+import java.util.Map;
 
 @Configuration
 @EnableCaching
 public class CacheConfig
 {
     @Bean
-    public CacheManager cacheManager() {
-        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues()
+                .serializeKeysWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
 
-        caffeineCacheManager.registerCustomCache("content",
-                Caffeine.newBuilder()
-                        .maximumSize(500)
-                        .expireAfterWrite(10, TimeUnit.MINUTES)
-                        .recordStats()
-                        .build());
+        Map<String, RedisCacheConfiguration> cacheConfigs = Map.of(
+                "content", defaultConfig.entryTtl(Duration.ofMinutes(10)),
+                "contents", defaultConfig.entryTtl(Duration.ofMinutes(5))
+        );
 
-        caffeineCacheManager.registerCustomCache("contents",
-                Caffeine.newBuilder()
-                        .maximumSize(100)
-                        .expireAfterWrite(5, TimeUnit.MINUTES)
-                        .recordStats()
-                        .build());
-
-        return new TransactionAwareCacheManagerProxy(caffeineCacheManager);
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(defaultConfig.entryTtl(Duration.ofMinutes(10)))
+                .withInitialCacheConfigurations(cacheConfigs)
+                .build();
     }
 }

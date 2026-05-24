484b921740f3989fe6a93aadd0132ee677e48a22
kyeongtae kim <130894141+xxzeroeight@users.noreply.github.com>
2026-05-25 04:33:49 +0900

    refactor: migrate Elasticsearch to OpenSearch (#114)

--- src/main/java/com/mopl/mopl/MoplApplication.java
+++ src/main/java/com/mopl/mopl/MoplApplication.java
@@ -2,8 +2,9 @@ package com.mopl.mopl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ElasticsearchDataAutoConfiguration.class})
 public class MoplApplication {
 
 	public static void main(String[] args) {

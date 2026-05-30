cdb177346ea535baf06acda06a34424db70a7c29
xxzeroeight <xxzeroeight@naver.com>
2026-05-30 16:58:55 +0900

    feat: vector-based personalized content recommendation settings

--- src/main/java/com/mopl/mopl/MoplApplication.java
+++ src/main/java/com/mopl/mopl/MoplApplication.java
@@ -2,9 +2,8 @@ package com.mopl.mopl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 
-@SpringBootApplication(exclude = {ElasticsearchDataAutoConfiguration.class})
+@SpringBootApplication
 public class MoplApplication {
 
 	public static void main(String[] args) {
